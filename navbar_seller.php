fac658fb5950c844273ee5adfb33490c6d51dc3d
spikeleeroypatayon <124319922+spikeleeroypatayon@users.noreply.github.com>
2024-05-08 10:30:40 +0800

    navbar_seller.php

---
+++ navbar_seller.php
@@ -0,0 +1,36 @@
+<?php
+echo '<nav class="">
+<div class="">
+  <a href="https://flowbite.com/" class="">
+      <img src="pics/site_logo.png" class="" alt="Flowbite Logo" />
+      <span class="">Hustle Every Dye</span>
+  </a>
+  <button data-collapse-toggle="navbar-default" type="button" class="" aria-controls="navbar-default" aria-expanded="false">
+      <span class="">Open main menu</span>
+      <svg class="" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+      </svg>
+  </button>
+  <div class="" id="navbar-default">
+    <ul class="">
+      <li>
+        <a href="index.php" class="" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="products.php" class="">Products</a>
+      </li>
+      <li>
+        <a href="cart.php" class="">Cart</a>
+      </li>
+      <li>
+        <a href="contact_us.php" class="">Contact</a>
+      </li>
+    </ul>
+  </div>
+  <a href="seller_dashboard.php"
+  class="">
+   Seller Dashboard
+</a>
+</div>
+</nav>';
+?>
