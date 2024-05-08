713912d571dabe0c0a4d7e144a26460a3d8953de
spikeleeroypatayon <124319922+spikeleeroypatayon@users.noreply.github.com>
2024-05-08 09:51:24 +0800

    about.php

---
+++ about.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link href="src/output.css" rel="stylesheet">
+    <style>
+        .BorderLine {
+            border-bottom: 2px solid #000;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+        .sectionBorder {
+            border-left: 10px solid #f7c433;
+            padding-left: 10px;
+            margin-bottom: 20px;
+        }
+        .sectionHeading {
+            font-size: 1.5rem;
+            margin-bottom: 10px;
+        }
+        .sectionContent {
+            font-size: 1rem;
+            color: #666;
+        }
+        .whole-content{
+            height: 100%;
+        }
+    </style>
+</head>
+<body>
+<div class = "">
+    <div class="">
+        <h2 class="">About Us</h2>
+        <p class="">This page is under the management of Entrepreneur Ecommerce LLC
+           <br> We make Tie dye with ❤️.</p>
+        <div class="">
+            <section class=""><img src="pics/site_logo.png" /><h2 class="">Hustle Every Dye</h2></section>
+            <section class="">
+                <p>Together, we make an unstoppable team, driven by ambition, creativity, and a shared commitment to success.<br>Welcome to Hustle Every Dye, where elegance and style unite. Our brand, which was founded with the intention of redefining fashion, is the epitome of style and sophistication. Our store, which can be found away in the lively area of Ruwais in Abu Dhabi, is a destination for style fans looking for classic pieces that deftly combine tradition with contemporary trends.</p>
+            </section>
+        </div>
+        <div class=""></div>
+        <div class="">
+            <h2 class="">Privacy Policy</h2>
+            <p class="">We at Hustle Every Dye are dedicated to safeguarding the confidentiality and integrity of your personal data. When you visit our website or engage with us in any other way, your information is collected, used, disclosed, and protected according to this Privacy Policy.You authorize the gathering, utilization, and dissemination of your information as specified in this Privacy Policy by engaging in our services.
+<br>
+<br>
+    <b>Information We Collect, Use, Share:</b>
+    <br>
+    -> Personal Information: We may gather personal information about you, including your name, email address, phone number, billing and shipping address, and payment information, when you make a purchase, create an account, or communicate with us.
+    <br>
+    -> Cookies: To improve your online experience, track website traffic, and customize content, we use cookies and related tracking technologies. You control your options for cookies.
+    <br>
+   -> Offer and Enhance Services: We utilize your data to handle orders, get in touch with you, and customize your online purchasing experience. We also utilize it for trend analysis, performance monitoring, and feature development on our websites.
+    <br>
+    -> Legal Compliance: In accordance with the law or in response to valid demands from public authorities, including government agencies or law enforcement, we may divulge personal information.
+    <br>
+    -> Service Providers: In order to help us run our website, handle payments, complete transactions, and send out marketing materials, we could disclose your information to dependable third-party service providers.
+    <br>
+    <b>Information Security:</b>
+     We put in place appropriate security measures to guard your data against loss, modification, disclosure, and unauthorized access. Nevertheless, we are unable to provide a 100% secure transmission via the internet or electronic storage solution.
+    <br>
+    <b>You Have Rights:</b>
+    You are entitled to see, update, or remove your personal data. Additionally, you have the option to object to the use of your information for certain reasons or request that we limit how we treat it.
+    <br>
+    <br>
+    <b>You agree to the terms and conditions of this privacy policy by using our services going forward, and you acknowledge to having read and comprehended it.
+</b>
+        </p>
+        
+        </div>
+        <div class=""></div>
+        <div class="">
+            <h2 class="">Licensing</h2>
+            <p class="">4203435.01</p>
+        </div>
+        <div class=""></div>
+        <div class="">
+            <h2 class="">Contact</h2>
+            <p class="">Please get in contact with us at if you have any queries, comments, or suggestions at these fields:
+            <br>
+            Mobile number: 0569076218
+            <br>
+            Facebook Page: https://www.facebook.com/HustleEveryDye</p>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php';?>
+</body>
+</html>
