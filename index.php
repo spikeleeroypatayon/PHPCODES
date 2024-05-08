f94f08b31591a2612eaaa8ce9f7ef92339089148
spikeleeroypatayon <124319922+spikeleeroypatayon@users.noreply.github.com>
2024-05-08 10:21:34 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Index</title>
+<link href="src/output.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include 'navbar.php';?>
+
+
+
+<?php echo '<div class="">
+<div class="">
+    <div>
+        <img class="" src="pics/all_products.png" alt="">
+    </div>
+    <div class="">
+        <div>
+            <img class="" src="pics/1.png" alt="">
+        </div>
+        <div>
+            <img class="" src="pics/2.png" alt="">
+        </div>
+        <div>
+            <img class="" src="pics/3.png" alt="">
+        </div>
+        <div>
+            <img class="" src="pics/4.png" alt="">
+        </div>
+        <div>
+            <img class="" src="pics/5.png" alt="">
+        </div>
+    </div>
+</div>
+</div>'
+;?>
+
+<?php include 'footer.php';?>
+</body>
+</html>
