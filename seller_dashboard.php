5c66f65c32f544200a02a763d1cbc6e9a1cf4bee
spikeleeroypatayon <124319922+spikeleeroypatayon@users.noreply.github.com>
2024-05-08 10:42:51 +0800

    seller_dashboard.php

---
+++ seller_dashboard.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Index</title>
+<link href="src/output.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include 'navbar_seller.php';?>
+
+
+
+<div class="">
+    <table class="">
+        <thead class="">
+            <tr>
+                <th scope="col" class="">
+                    Product name
+                </th>
+                <th scope="col" class="">
+                    Quantity
+                </th>
+                <th scope="col" class="">
+                    Category
+                </th>
+                <th scope="col" class="">
+                    Price
+                </th>
+                <th scope="col" class="">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <th scope="row" class="">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="">
+                    2
+                </td>
+                <td class=">
+                    Laptop
+                </td>
+                <td class="">
+                    $2999
+                </td>
+                <td class="">
+                    <a href="#" class="">Edit</a>
+                </td>
+            </tr>
+            <tr class="">
+                <th scope="row" class="">
+                    Microsoft Surface Pro
+                </th>
+                <td class="">
+                    3
+                </td>
+                <td class="">
+                    Laptop PC
+                </td>
+                <td class="">
+                    $1999
+                </td>
+                <td class="">
+                    <a href="#" class="">Edit</a>
+                </td>
+            </tr>
+            <tr class="">
+                <th scope="row" class="">
+                    Magic Mouse 2
+                </th>
+                <td class="">
+                    1
+                </td>
+                <td class="">
+                    Accessories
+                </td>
+                <td class="">
+                    $99
+                </td>
+                <td class="">
+                    <a href="#" class="">Edit</a>
+                </td>
+            </tr>
+            <tr class="">
+                <th scope="row" class="">
+                    Google Pixel Phone
+                </th>
+                <td class="">
+                    4
+                </td>
+                <td class="">
+                    Phone
+                </td>
+                <td class=" ">
+                    $799
+                </td>
+                <td class=" ">
+                    <a href="#" class=" ">Edit</a>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" class="">
+                    Apple Watch 5
+                </th>
+                <td class="">
+                    3
+                </td>
+                <td class="">
+                    Wearables
+                </td>
+                <td class=">
+                    $999
+                </td>
+                <td class="">
+                    <a href="#" class="">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+<?php include 'footer.php';?>
+</body>
+</html>
