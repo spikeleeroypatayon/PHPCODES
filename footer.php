ebc4bf74edb97b4a2e4b18a68f53f2ca9e68edb7
spikeleeroypatayon <124319922+spikeleeroypatayon@users.noreply.github.com>
2024-05-08 10:20:17 +0800

    footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="">
+    <div class="">
+      <span class="/" class="">Hustle Every Dye™</a>. All Rights Reserved.
+    </span>
+    <ul class="">
+        <li>
+            <a href="about.php" class="">About</a>
+        </li>
+        <li>
+            <a href="about.php" class="">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="about.php" class="">Licensing</a>
+        </li>
+        <li>
+            <a href="about.php" class="">Contact</a>
+        </li>
+    </ul>
+    </div>
+</footer>
